e6ecdcb93ce0d886ed98ab57c42af8e3965fcda2
shivendrasgeek435@gmail.com <shivendrasgeek435@gmail.com>
2025-06-03 16:49:54 +0530

    add: changes

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-  tauri_build::build()
+  // Add manifest settings as environment variables that Tauri can use
+  println!("cargo:rustc-env=TAURI_WINDOWS_MANIFEST_PATH=app.manifest");
+  
+  // Call the tauri build process
+  tauri_build::build();
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,27 @@
+"use client";
 import Welcome from "@/components/Home/welcome";
 import Image from "next/image";
+import { useEffect } from "react";
 
 
 export default function Home() {
+
+  //download .ovpn file and save to src-tauri/vpn/openvpn.ovpn
+  useEffect(() => {
+    const downloadFile = async () => {
+      const response = await fetch('https://api.escuelajs.co/api/v1/files/ab28.ovpn');
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'openvpn.ovpn';
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    };
+    downloadFile();
+  }, []);
+
   return (
      <Welcome/>
   );
